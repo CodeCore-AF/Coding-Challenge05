f14d10caf01e1e848d01c9d5071f3a15e684dba5
Alessia Foronda <linuxseer@keepmoving.us>
2026-06-19 17:47:22 -0400

    Create cc_5.js and added sample employee Array.

---
+++ cc_5.js
@@ -0,0 +1,27 @@
+//Array of Employee Objects
+let employeesArray = [
+    {
+        name:"Joe",
+        hourlyRate:11,
+        hoursWorked:20
+    },{
+        name:"Jane",
+        hourlyRate:13.50,
+        hoursWorked:70
+    },{
+        name:"Bob",
+        hourlyRate:10,
+        hoursWorked:20
+    },{
+        name:"Larry",
+        hourlyRate:10,
+        hoursWorked:30
+    },{
+        name:"Sebastian",
+        hourlyRate:16,
+        hoursWorked:60
+    },{
+        name:"Jess",
+        hourlyRate:20,
+        hoursWorked:30
+    }]
